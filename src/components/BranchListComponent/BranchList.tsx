58ac6285fde45c2c39ce96aa0d65ed8c955a3e42
mehedi <mhrmehedi18@gmail.com>
2023-12-28 12:38:16 +0600

    branch list ui design fixed

--- src/components/BranchListComponent/BranchList.tsx
+++ src/components/BranchListComponent/BranchList.tsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import { Space, Table, Popover, Button } from "antd";
 import type { ColumnsType } from "antd/es/table";
 import { GoGear } from "react-icons/go";
+import { MdDelete } from "react-icons/md";
+import { AiTwotoneCheckCircle } from "react-icons/ai";
+import { AiTwotoneCloseCircle } from "react-icons/ai";
 
 interface DataType {
   key: string;
@@ -55,11 +58,32 @@ export const BranchList = () => {
 
   const content = (record: DataType) => (
     <div className="border-t-[1px] border-gray-200">
-      <div className="m-2 flex justify-between gap-2">
-        <Button onClick={() => handleToggle(record.key)}>
-          {record.status === "Enable" ? "Disable" : "Enable"}
-        </Button>
-        <Button onClick={() => handleDelete(record.key)}>Delete</Button>
+      <div className="m-2 flex justify-evenly">
+        <button onClick={() => handleToggle(record.key)}>
+          {record.status === "Enable" ? (
+            <button className="bg-red-500 hover:bg-red-400 active:bg-red-500 px-2 py-1 rounded text-white transition">
+              <div className="flex items-center">
+                <AiTwotoneCloseCircle /> Disable
+              </div>
+            </button>
+          ) : (
+            <button className="bg-green-500 hover:bg-green-400 active:bg-green-500 px-2 py-1 rounded text-white transition">
+              <div className="flex items-center">
+                <AiTwotoneCheckCircle /> Enable
+              </div>
+            </button>
+          )}
+        </button>
+        {/* <Button onClick={() => handleDelete(record.key)} >Delete</Button> */}
+        <button
+          onClick={() => handleDelete(record.key)}
+          className="bg-red-500 hover:bg-red-400 active:bg-red-500 px-2 py-1 rounded text-white transition"
+        >
+          <div className="flex items-center">
+            <MdDelete />
+            Delete
+          </div>
+        </button>
       </div>
     </div>
   );
@@ -110,21 +134,16 @@ export const BranchList = () => {
   ];
 
   return (
-    <div className="bg-slate-100 rounded-lg">
-      <div className="m-[80px] mt-1">
-        <div className="flex justify-center items-center">
-          <div className="w-full m-10 ">
-            <h2 className=" text-[20px] text-black p-5 bg-white rounded-t-lg">
-              Restaurant Branch List
-            </h2>
-            <Table
-              columns={columns}
-              dataSource={data}
-              style={{ borderRadius: 0 }}
-            />
-          </div>
-        </div>
-      </div>
+    <div className=" bg-gray-100 min-h-[calc(100vh-(130px))] rounded-lg pt-5 overflow-x-scroll">
+      <h2 className="bg-white mx-5 font-bold text-lg p-5 rounded-lg">
+        Restaurant Branch List
+      </h2>
+      <Table
+        className="mx-5"
+        columns={columns}
+        dataSource={data}
+        style={{ borderRadius: 0 }}
+      />
     </div>
   );
 };
--- src/pages/branches/index.tsx
+++ src/pages/branches/index.tsx
@@ -3,11 +3,7 @@ import { BranchList } from "./../../components/BranchListComponent/BranchList";
 
 const index = () => {
   return (
-    <div className="sm:px-5 px-3">
-      <div className="bg-gray-100 rounded-lg">
         <BranchList />
-      </div>
-    </div>
   );
 };
 
