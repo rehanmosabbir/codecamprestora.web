8c21b2d80777defb6ce08aca08dc4b678b2e3cbb
Ahmad Hossain <ahmadhossain.bu@gmail.com>
2024-01-02 12:57:46 +0600

    style : Layout ( login, registration ) change

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -2,6 +2,7 @@ import React, { ReactNode } from "react";
 import { Button, Layout, theme } from "antd";
 
 import LeftMenuBar from "./LeftMenuBar";
+import { useRouter } from "next/router";
 
 const { Header, Content, Sider, Footer } = Layout;
 
@@ -9,41 +10,54 @@ const RootLayout = ({ children }: { children: ReactNode }) => {
   const {
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
+  const router = useRouter();
+
+  const isShow =
+    router.asPath !== "/login" && router.asPath !== "/registration";
 
   return (
     <Layout>
-      <Header
-        style={{
-          position: "sticky",
-          top: 0,
-          zIndex: 1,
-          width: "100%",
-          display: "flex",
-          alignItems: "center",
-          background: colorBgContainer,
-        }}
-      ></Header>
+      {isShow && (
+        <Header
+          style={{
+            position: "sticky",
+            top: 0,
+            zIndex: 1,
+            width: "100%",
+            display: "flex",
+            alignItems: "center",
+            background: colorBgContainer,
+          }}
+        ></Header>
+      )}
       <Layout
         hasSider
-        style={{ paddingRight: "20px", background: colorBgContainer }}
+        style={{
+          paddingRight: `${isShow ? "20px" : ""}`,
+          background: colorBgContainer,
+        }}
       >
-        <Sider
-          width="17%"
-          style={{
-            overflow: "auto",
-            height: "100vh",
-            position: "fixed",
-            background: colorBgContainer,
-            top: 60,
-            left: 0,
-          }}
-        >
-          <LeftMenuBar />
-        </Sider>
+        {isShow && (
+          <Sider
+            width="17%"
+            style={{
+              overflow: "auto",
+              height: "100vh",
+              position: "fixed",
+              background: colorBgContainer,
+              top: 60,
+              left: 0,
+            }}
+          >
+            <LeftMenuBar />
+          </Sider>
+        )}
         <Layout style={{ padding: "5px" }}>
           <Content
             style={{
-              marginLeft: "17%",
+              marginLeft: `${isShow ? "19%" : ""}`,
+              marginTop: `${isShow ? "15px" : ""}`,
+              marginRight: `${isShow ? "15px" : ""}`,
               minHeight: 280,
               background: colorBgContainer,
               borderRadius: borderRadiusLG,
@@ -51,7 +65,12 @@ const RootLayout = ({ children }: { children: ReactNode }) => {
           >
             {children}
           </Content>
-          <Footer style={{ marginLeft: "17%", textAlign: "center" }}>
+          <Footer
+            style={{
+              marginLeft: `${isShow ? "17%" : ""}`,
+              textAlign: "center",
+            }}
+          >
             Restora App ©2023
           </Footer>
         </Layout>
