b81776fbf9bc1d1a2cc701203b241fb1c526d726
Ahmad Hossain <ahmadhossain.bu@gmail.com>
2024-01-14 11:50:13 +0600

    feat: Branch Pictures dnd add

--- src/pages/branches/[branchid]/pictures/index.tsx
+++ src/pages/branches/[branchid]/pictures/index.tsx
@@ -1,5 +1,75 @@
+import { useState } from "react";
+import {
+  DndContext,
+  closestCenter,
+  MouseSensor,
+  TouchSensor,
+  DragOverlay,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import {
+  arrayMove,
+  SortableContext,
+  rectSortingStrategy,
+} from "@dnd-kit/sortable";
+
+import { Grid } from "@/components/Grid";
+import { SortablePhoto } from "@/components/SortablePhoto";
+import { Photo } from "@/components/Photo";
+import photos from "../../../../data/photos.json";
+
 const BranchPicturesPage = () => {
-  return <div>BranchPicturesPage</div>;
+  const [items, setItems] = useState(photos);
+  const [activeId, setActiveId] = useState(null);
+  const sensors = useSensors(useSensor(MouseSensor), useSensor(TouchSensor));
+
+  return (
+    <DndContext
+      sensors={sensors}
+      collisionDetection={closestCenter}
+      onDragStart={handleDragStart}
+      onDragEnd={handleDragEnd}
+      onDragCancel={handleDragCancel}
+    >
+      <SortableContext items={items} strategy={rectSortingStrategy}>
+        <Grid columns={4}>
+          {items.map((url: any, index: any) => (
+            <SortablePhoto key={url} url={url} index={index} />
+          ))}
+        </Grid>
+      </SortableContext>
+
+      <DragOverlay adjustScale={true}>
+        {activeId ? (
+          <Photo url={activeId} index={items.indexOf(activeId)} />
+        ) : null}
+      </DragOverlay>
+    </DndContext>
+  );
+
+  function handleDragStart(event: any) {
+    setActiveId(event.active.id);
+  }
+
+  function handleDragEnd(event: any) {
+    const { active, over } = event;
+
+    if (active.id !== over.id) {
+      setItems((items: any) => {
+        const oldIndex = items.indexOf(active.id);
+        const newIndex = items.indexOf(over.id);
+
+        return arrayMove(items, oldIndex, newIndex);
+      });
+    }
+
+    setActiveId(null);
+  }
+
+  function handleDragCancel() {
+    setActiveId(null);
+  }
 };
 
 export default BranchPicturesPage;
