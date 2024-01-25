398dcfc39a9ac0be4edded6ef2e0322d428d7a8b
i_am_mehedi <mehedihasan.ist25.com>
2024-01-25 13:00:47 +0600

    change branch issue fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import RestaurantUsersList from "@/components/RestaurantUsersList/RestaurantUsersList";
 import { useSession } from "next-auth/react";
 
 export default function Home() {
@@ -8,7 +7,6 @@ export default function Home() {
       User Logged in : {JSON.stringify(data)}
       <h2 className="text-3xl font-bold text-center pt-[40vh] pb-[40vh]">
         Welcome to Restora.App
-        <RestaurantUsersList></RestaurantUsersList>
       </h2>
     </main>
   );
