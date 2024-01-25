ea4cad36a48e152d23c2e17bb536a69d39c91c43
i_am_mehedi <mehedihasan.ist25.com>
2024-01-25 16:58:47 +0600

    fix: retrieved missing codes and resolved breaking changes

--- src/components/BranchCreationComponent/BranchInfoEdit.tsx
+++ src/components/BranchCreationComponent/BranchInfoEdit.tsx
@@ -2,16 +2,16 @@ import { Card } from "antd";
 import Title from "antd/es/typography/Title";
 import { BranchInformationForm } from "./BranchInformationForm";
 
-export const BranchInfoEdit = (props: {
-  formClose: React.Dispatch<React.SetStateAction<boolean>>,
-  branchID:string | undefined;
+export const BranchInfoEdit = ({
+  formClose,
+}: {
+  formClose: React.Dispatch<React.SetStateAction<boolean>>;
 }) => {
- const { formClose,branchID} = props;
-  
+  console.log({ formClose });
   return (
     <div className="w-full">
       <Card title={<Title className="pt-5 pb-2" level={4}>Branch Information</Title>}>
-        <BranchInformationForm formClose ={formClose} branchID={branchID} ></BranchInformationForm>
+        <BranchInformationForm formClose={formClose}></BranchInformationForm>
       </Card>
     </div>
   );
